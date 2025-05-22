0f79d7f942852a3dd99992f32c4070a3a7e9b0df
jarvondigital <jarvondigital@gmail.com>
2025-05-21 20:03:10 -0400

    - Updated the design - Added Animations - Cleaned up some media queries

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideLottieOptions } from 'ngx-lottie';
+import player from 'lottie-web'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideLottieOptions({
+      player: () => player,
+    })
   ],
 };
--- src/app/components/feature-blocks/feature-blocks.component.ts
+++ src/app/components/feature-blocks/feature-blocks.component.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
 
 @Component({
   selector: 'app-feature-blocks',
   standalone: true,
+  imports: [LottieComponent],
   templateUrl: './feature-blocks.component.html',
-  styleUrls: ['./feature-blocks.component.scss']
+  styleUrls: ['./feature-blocks.component.scss'],
 })
-export class FeatureBlocksComponent {} 
+export class FeatureBlocksComponent {
+  pressureOptions: AnimationOptions = {
+    path: 'lottieflow-attention-06-000000-easey.json',
+    loop: true,
+    autoplay: true,
+  };
+
+  trashOptions: AnimationOptions = {
+    path: 'lottieflow-ecommerce-14-19-000000-easey.json',
+    loop: true,
+    autoplay: true,
+  };
+
+  guaranteeOptions: AnimationOptions = {
+    path: 'lottieflow-success-05-000000-easey.json',
+    loop: true,
+    autoplay: true,
+  };
+
+  lottieStyles = {
+    width: '80px',
+    height: '80px',
+  };
+}
